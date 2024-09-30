949869500446ddffcb230ca7b8d79ea6854ff3f8
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2024-09-30 16:24:16 +0300

    created starter files

---
+++ script.js
@@ -0,0 +1,11 @@
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const multiply = function(arr) {
+    return arr.reduce((a,b) => a * b, 1);
+  };
