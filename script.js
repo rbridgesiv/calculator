dec977c159b391d906787a97bd7d4e62363e9a5b
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2024-11-15 15:21:20 +0200

    created final layout; no functionality yet

--- script.js
+++ script.js
@@ -7,5 +7,42 @@ const subtract = function(a,b) {
 };
 
 const multiply = function(arr) {
-    return arr.reduce((a,b) => a * b, 1);
+    return a * b;
   };
+
+const divide = function(a, b){
+    if (a === 0 || b === 0) {return undefined;}
+    return a / b;
+}
+
+let firstNumber
+
+let operator
+
+let secondNumber
+
+let displayValue
+
+const operate = function(operator, firstNumber, secondNumber){
+    switch (operator) {
+        case 'add':
+            return add(firstNumber,secondNumber);
+            break;
+        case 'subtract':
+            return subtract(firstNumber,secondNumber);
+            break;
+        case 'multiply':
+            return multiply(firstNumber,secondNumber);
+            break;
+        case 'divide':
+            return divide(firstNumber,secondNumber);
+            break;
+        default:
+            return 'Invalid';
+    }
+}
+
+const displayText= function() {
+    let text = document.getElementById("textField");
+    text.style.display = "";
+  }
